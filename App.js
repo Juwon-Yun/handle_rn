866723b3fb4a542776280432224652655a633474
Juwon-Yun <eooll2@daum.net>
2022-09-14 23:05:15 +0900

    feature: AppLoading expo

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import AppLoading from 'expo-app-loading';
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text} from 'react-native';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const onFinish = () => setIsLoading(true);
+  const startLoading = async () => {
+    await new Promise(resolve => setTimeout(resolve, 5000));
+  };
+
+  if(!isLoading){
+    return (
+      <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.error} />
+    );
+  }
   return (
-    null
+  <SafeAreaView>
+    <View>
+      <Text>
+        Loading Done
+      </Text>
+    </View>
+  </SafeAreaView>
   );
 }
 
