f6d1bd87aa8ce48a702d4def9e1b91e388f13803
Juwon-Yun <eooll2@daum.net>
2022-09-15 22:39:13 +0900

    feature: Font, Assets, Image prefetch

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import AppLoading from 'expo-app-loading';
 import React, { useState } from 'react';
-import { SafeAreaView, View, Text} from 'react-native';
+import { SafeAreaView, View, Text, Image} from 'react-native';
+
+//https://docs.expo.dev/guides/icons/
+import * as Font from 'expo-font';
+import {Ionicons} from "@expo/vector-icons"
+import {Asset} from 'expo-asset';
+
 // import * as SplashScreen from 'expo-splash-screen';
 
 
@@ -10,7 +16,12 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const onFinish = () => setIsLoading(true);
   const startLoading = async () => {
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    await Font.loadAsync(Ionicons.font)
+
+    // 2 type preload : local assets or assets API
+    await Asset.loadAsync(require('./s.png'));
+
+    await Image.prefetch('https://yt3.ggpht.com/IdRkd4wCZXxbxMG8mbWhCF7nxiypaO1w6EQ6RbtBn02R31MIY2z8FEqbHs0FDQERkBYqZlXAnFU=s48-c-k-c0x00ffffff-no-rj')
   };
 
   if(!isLoading){
