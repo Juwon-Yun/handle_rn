7512a7ca3b8c23bff0397e19c1f3a4d5a0fff2cd
Juwon-Yun <eooll2@daum.net>
2022-09-17 20:52:32 +0900

    feature: useAssets

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { SafeAreaView, View, Text, Image} from 'react-native';
 //https://docs.expo.dev/guides/icons/
 import * as Font from 'expo-font';
 import {Ionicons} from "@expo/vector-icons"
-import {Asset} from 'expo-asset';
+import {Asset, useAssets} from 'expo-asset';
 
 // import * as SplashScreen from 'expo-splash-screen';
 
@@ -23,22 +23,30 @@ const loadAssets = (images) => images.map(image => {
 })
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const onFinish = () => setIsLoading(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Font.loadAsync(Ionicons.font), Font.loadAsync(Ionicons.font)]);
-    // 2 type preload : local assets or assets API
+  // const [isLoading, setIsLoading] = useState(false);
+  // const onFinish = () => setIsLoading(true);
+  // const startLoading = async () => {
+  //   const fonts = loadFonts([Font.loadAsync(Ionicons.font), Font.loadAsync(Ionicons.font)]);
+  //   // 2 type preload : local assets or assets API
 
-    const images = loadAssets([require('./s.png'), 'https://yt3.ggpht.com/IdRkd4wCZXxbxMG8mbWhCF7nxiypaO1w6EQ6RbtBn02R31MIY2z8FEqbHs0FDQERkBYqZlXAnFU=s48-c-k-c0x00ffffff-no-rj']);
+  //   const images = loadAssets([require('./s.png'), 'https://yt3.ggpht.com/IdRkd4wCZXxbxMG8mbWhCF7nxiypaO1w6EQ6RbtBn02R31MIY2z8FEqbHs0FDQERkBYqZlXAnFU=s48-c-k-c0x00ffffff-no-rj']);
 
-    await Promise.all([...fonts, ...images])
-  };
+  //   await Promise.all([...fonts, ...images])
+  // };
 
-  if(!isLoading){
-    return (
-      <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.error} />
-    );
+  // if(!isLoading){
+  //   return (
+  //     <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.error} />
+  //   );
+  // }
+
+  const [assets] = useAssets([require('./s.png'), 'https://yt3.ggpht.com/IdRkd4wCZXxbxMG8mbWhCF7nxiypaO1w6EQ6RbtBn02R31MIY2z8FEqbHs0FDQERkBYqZlXAnFU=s48-c-k-c0x00ffffff-no-rj']);
+
+  if(!assets){
+    return <AppLoading/>
   }
+
+
   return (
   <SafeAreaView>
     <View>
