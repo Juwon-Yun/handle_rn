4126e5b1a85b9ca6c5e2d2112cfe723b30b11936
Juwon-Yun <eooll2@daum.net>
2022-09-19 23:01:03 +0900

    refactor: constants color

---
+++ constants/colors.js
@@ -0,0 +1,6 @@
+export const YELLOW_COLOR = '#ffa801'
+export const BLACK_COLOR = '#1e272e'
+
+// const colors = {}
+
+// export default colors;
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,5 +1,6 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import { View, Text} from 'react-native';
+import { View, Text, useColorScheme} from 'react-native';
+import { BLACK_COLOR, YELLOW_COLOR } from '../constants/colors';
 import Movie from '../screens/Movie';
 import Search from '../screens/Search';
 import Tv from '../screens/Tv';
@@ -8,36 +9,52 @@ import Tv from '../screens/Tv';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-<Tab.Navigator 
-    initialRouteName='Movie'
-    screenOptions={{
-    //     tabBarLabelStyle:{
-    //         backgroundColor : 'red'
-    //     }
-        // tabBarLabelPosition:'beside-icon'
-        // tabBarActiveTintColor : 'red',
-        // tabBarInactiveTintColor : 'purple'
-        // tabBarStyle: {
-        //     backgroundColor : 'tomato'
-        // }
-        // headerTitleStyle : {
-        //     color : 'tomato',
-        //     backgroundColor : 'black'
-        // },
-        // headerRight : () => (<View><Text>HeaderRight</Text></View>),
-    }}
-    >
-    <Tab.Screen name='Movie' component={Movie}/>
-    <Tab.Screen name='Tv' component={Tv}
-      options={{
-    //     tabBarLabelStyle:{
-    //         backgroundColor : 'red'
-    //     }
-        // tabBarBadge : 190
+const Tabs = () => {
+  // cmd + shift + a
+  const isDark = useColorScheme() === 'dark';
+
+  return (
+    <Tab.Navigator 
+      initialRouteName='Movie'
+      screenOptions={{
+        tabBarStyle:{
+          backgroundColor : isDark ? BLACK_COLOR : 'white'
+        },
+        tabBarActiveTintColor : isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor : isDark ? '#d2dae2' : '#d2dae2',
+        headerStyle : {
+          backgroundColor : isDark ? BLACK_COLOR : 'white'
+        },
+        headerTitleStyle : {
+          color : isDark ? YELLOW_COLOR : 'black'
+        }
+      //     tabBarLabelStyle:{ 
+      //         backgroundColor : 'red'
+      //     }
+          // tabBarLabelPosition:'beside-icon'
+          // tabBarActiveTintColor : 'red',
+          // tabBarInactiveTintColor : 'purple'
+          // tabBarStyle: {
+          //     backgroundColor : 'tomato'
+          // }
+          // headerTitleStyle : {
+          //     color : 'tomato',
+          //     backgroundColor : 'black'
+          // },
+          // headerRight : () => (<View><Text>HeaderRight</Text></View>),
       }}
-     />
-    <Tab.Screen name='Search' component={Search}/>
-</Tab.Navigator>);
+      >
+      <Tab.Screen name='Movie' component={Movie}/>
+      <Tab.Screen name='Tv' component={Tv}
+        options={{
+      //     tabBarLabelStyle:{
+      //         backgroundColor : 'red'
+      //     }
+          // tabBarBadge : 190
+        }}
+      />
+      <Tab.Screen name='Search' component={Search}/>
+    </Tab.Navigator>)
+    }
 
 export default Tabs;
