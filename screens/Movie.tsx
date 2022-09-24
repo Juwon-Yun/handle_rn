5fb17465d7c5946c427e8fc723a615afbb71f4f5
Juwon-Yun <eooll2@daum.net>
2022-09-24 22:11:42 +0900

    feature: rn web swiper

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -3,10 +3,7 @@ import React from "react";
 // import {Text, TouchableOpacity} from 'react-native'
 import styled from "styled-components/native";
 
-const Btn = styled.TouchableOpacity`
-    flex : 1;
-    justify-content : center;
-    align-items : center; 
+const CustomScrollView = styled.ScrollView`
     background-color: ${props => props.theme.mainBgColor}
 `;
 
@@ -15,22 +12,25 @@ const Btn = styled.TouchableOpacity`
 // 코드를 컴파일하기 때문에 에러가 있으면 컴파일 하지 못하지만, 
 // rn-ts에서의 typescript는 도와주는 역할이라 에러를 무시해도 앱이 실행된다.
 
-const Title = styled.Text`
-    color : ${(props) => props.theme.textColor};
-`;
-    // color : ${(props) => props.selected ? "blue" : "red"};
+// https://reactnavigation.org/docs/typescript/#type-checking-screens
+const Movie : React.FC<NativeStackScreenProps<any, 'Movie'>> = () => (
+    <CustomScrollView>
+
+    </CustomScrollView>
+);
+
 
 // https://reactnavigation.org/docs/typescript/#type-checking-screens
-const Movie : React.FC<NativeStackScreenProps<any, 'Movie'>> = ({navigation : {navigate}}) => <Btn
+// const Movie : React.FC<NativeStackScreenProps<any, 'Movie'>> = ({navigation : {navigate}}) => <Btn
     // style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}
     // Tab Nav에서 Stack Nav로 가려고하기 때문에
     // 다른 Nav 끼리는 Nav명을 명시해줘야 한다.
     // onPress={ () => navigate("Three")}
+    // 
+    // onPress={ () =>  navigate("Stack", {screen : "Three"})}
+    // >   
     
-    onPress={ () =>  navigate("Stack", {screen : "Three"})}
-    >   
-    
-        <Title selected={true}> Movie !!</Title>
-    </Btn>
+        {/* <Title selected={true}> Movie !!</Title> */}
+    {/* </Btn> */}
 
 export default Movie;
