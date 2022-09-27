41479a9a1d728afd14640d5d5f544a2d771142c1
Juwon-Yun <eooll2@daum.net>
2022-09-27 22:15:51 +0900

    modify: Movie.tsx

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,10 +1,13 @@
 import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import { Dimensions } from "react-native";
+// https://github.com/reactrondev/react-native-web-swiper
 import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
 
 const {height : swiperHeight} = Dimensions.get("window");
+
+const API_KEY = '34dbe792c998f87663d41737eb203cb2';
 
 const CustomScrollView = styled.ScrollView`
     background-color: ${props => props.theme.mainBgColor}
@@ -20,15 +23,19 @@ const View = styled.View`
 // rn-ts에서의 typescript는 도와주는 역할이라 에러를 무시해도 앱이 실행된다.
 
 // https://reactnavigation.org/docs/typescript/#type-checking-screens
-const Movie : React.FC<NativeStackScreenProps<any, 'Movie'>> = () => (
+const Movie : React.FC<NativeStackScreenProps<any, 'Movie'>> = () =>{ 
+    const getNowPlaying = () => {
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`);
+    };
+
+    return (
     <CustomScrollView>
         <Swiper loop timeout={3.5} controlsEnabled={false} containerStyle={{width : "100%", height : swiperHeight * 0.25}}>
             <View style={{backgroundColor : "red"}}></View>
             <View style={{backgroundColor : "blue"}}></View>
             <View style={{backgroundColor : "white"}}></View>
         </Swiper>
-    </CustomScrollView>
-);
+    </CustomScrollView>)};
 
 
 // https://reactnavigation.org/docs/typescript/#type-checking-screens
