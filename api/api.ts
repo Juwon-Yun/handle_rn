67890aa21d9d3d95c3c6054dded6a6d734b5b611
Juwon-Yun <eooll2@daum.net>
2022-10-06 22:33:40 +0900

    add: api.ts

---
+++ api/api.ts
@@ -0,0 +1,14 @@
+const API_KEY = '34dbe792c998f87663d41737eb203cb2';
+const baseUrl = 'https://api.themoviedb.org/3/';
+
+export const trending = () => fetch(`${baseUrl}trending/movie/week?api_key=${API_KEY}`).then(
+        response => response.json()
+    );
+
+export const upcomming = () => fetch(`${baseUrl}movie/upcoming?api_key=${API_KEY}`).then(
+        response => response.json()
+    );
+
+export const nowPlaying = () => fetch(`${baseUrl}movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`).then(
+        response => response.json()
+    );
