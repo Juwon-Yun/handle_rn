d957fa3d7c7e32a9896b79db5b501a6d2a21f601
Juwon-Yun <eooll2@daum.net>
2022-10-11 21:52:32 +0900

    modify: Tv.tsx

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import {View, Text} from 'react-native'
+import {View, Text, ScrollView, FlatList} from 'react-native'
 import { useQuery } from "react-query";
 import { tvApi } from "../api/api";
+import Loader from "../components/Loader";
+import VMedia from "./VMedia";
 
 const Tv = () => {
     const {isLoading : todayLoading, data : todayData } = useQuery(["tv", "today"], tvApi.airingToday);
@@ -10,7 +12,53 @@ const Tv = () => {
 
     const loading = todayLoading || topLoading || trandingLoading;
 
-    return <View style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}><Text> TV </Text></View>
+    if(loading){
+        return (<Loader />)
+    }
+
+    return (
+        <ScrollView>
+             <FlatList 
+                data={trandingData.results}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({item}) => (
+                    <VMedia 
+                        posterPath={item.poster_path}
+                        originalTitle={item.original_name}
+                        voteAverage={item.vote_average}
+                    />
+                )}
+            />
+            <FlatList 
+                data={todayData.results}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({item}) => (
+                    <VMedia 
+                        posterPath={item.poster_path}
+                        originalTitle={item.original_name}
+                        voteAverage={item.vote_average}
+                    />
+                )}
+            />
+             <FlatList 
+                data={topData.results}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({item}) => (
+                    <VMedia 
+                        posterPath={item.poster_path}
+                        originalTitle={item.original_name}
+                        voteAverage={item.vote_average}
+                    />
+                )}
+            />
+        </ScrollView>
+        // <View style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}>
+        //     <Text> TV </Text>
+        // </View>
+    )
 }
 
 export default Tv
