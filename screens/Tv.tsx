2b9cbbda4774621ae6f55b5e6baed5c95aec09fa
Juwon-Yun <eooll2@daum.net>
2022-10-11 21:42:07 +0900

    modify: tv.tsx

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import {View, Text} from 'react-native'
+import { useQuery } from "react-query";
+import { tvApi } from "../api/api";
 
-const Tv = () => <View style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}><Text> TV </Text></View>
+const Tv = () => {
+    const {isLoading : todayLoading, data : todayData } = useQuery(["tv", "today"], tvApi.airingToday);
+    const {isLoading : topLoading, data : topData } = useQuery(["tv", "top"], tvApi.topRated);
+    const {isLoading : trandingLoading, data : trandingData } = useQuery(["tv", "tranding"], tvApi.trending);
+
+    const loading = todayLoading || topLoading || trandingLoading;
+
+    return <View style={{flex : 1, justifyContent : 'center', alignItems : 'center'}}><Text> TV </Text></View>
+}
 
 export default Tv
